8e58bd32894b9e8e79188a6fb389f2b84ea17b02
nikitayp <147061112+nikitayp@users.noreply.github.com>
2023-10-23 23:31:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,58 @@
+# Importing Libraries
+import pandas as pd
+import mysql.connector as sql
+import streamlit as st
+import plotly.express as px
+import os
+import json
+from streamlit_option_menu import option_menu
+from PIL import Image
+from git.repo.base import Repo
+
+# Setting up page configuration
+icon = Image.open("ICN.png")
+st.set_page_config(page_title= "Phonepe Pulse Data Visualization | By Jafar Hussain",
+                   page_icon= icon,
+                   layout= "wide",
+                   initial_sidebar_state= "expanded",
+                   menu_items={'About': """# This dashboard app is created by *Jafar Hussain*!
+                                        Data has been cloned from Phonepe Pulse Github Repo"""})
+
+st.sidebar.header(":wave: :violet[**Hello! Welcome to the dashboard**]")
+
+# #To clone the Github Pulse repository use the following code
+# Reference Syntax - Repo.clone_from("Clone Url", "Your working directory")
+# Repo.clone_from("https://github.com/PhonePe/pulse.git", "Project_3_PhonepePulse/Phonepe_data/data")
+
+# Creating connection with mysql workbench
+mydb = sql.connect(host="localhost",
+                   user="root",
+                   password="Jafar@1996",
+                   database= "phonepe_pulse"
+                  )
+mycursor = mydb.cursor(buffered=True)
+
+
+# Creating option menu in the side bar
+with st.sidebar:
+    selected = option_menu("Menu", ["Home","Top Charts","Explore Data","About"], 
+                icons=["house","graph-up-arrow","bar-chart-line", "exclamation-circle"],
+                menu_icon= "menu-button-wide",
+                default_index=0,
+                styles={"nav-link": {"font-size": "20px", "text-align": "left", "margin": "-2px", "--hover-color": "#6F36AD"},
+                        "nav-link-selected": {"background-color": "#6F36AD"}})
+# MENU 1 - HOME
+if selected == "Home":
+    st.image("img.png")
+    st.markdown("# :violet[Data Visualization and Exploration]")
+    st.markdown("## :violet[A User-Friendly Tool Using Streamlit and Plotly]")
+    col1,col2 = st.columns([3,2],gap="medium")
+    with col1:
+        st.write(" ")
+        st.write(" ")
+        st.markdown("### :violet[Domain :] Fintech")
+        st.markdown("### :violet[Technologies used :] Github Cloning, Python, Pandas, MySQL, mysql-connector-python, Streamlit, and Plotly.")
+        st.markdown("### :violet[Overview :] In this streamlit web app you can visualize the phonepe pulse data and gain lot of insights on transactions, number of users, top 10 state, district, pincode and which brand has most number of users and so on. Bar charts, Pie charts and Geo map visualization are used to get some insights.")
+    with col2:
+        st.image("home.png")
+        
